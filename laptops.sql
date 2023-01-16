e529eaee9dffcb7bc8828ea9e6f5238a7d022c96
Mahesafy <mahesaramadhan11@gmail.com>
2023-01-16 22:16:52 +0700

    Update type data DATE dari Text Menjadi DATE

--- laptops.sql
+++ laptops.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Waktu pembuatan: 15 Jan 2023 pada 20.07
+-- Waktu pembuatan: 16 Jan 2023 pada 18.15
 -- Versi server: 8.0.31
 -- Versi PHP: 7.4.33
 
@@ -56,7 +56,7 @@ CREATE TABLE `laptops` (
   `memory` int NOT NULL,
   `storage` varchar(255) NOT NULL,
   `graphics` varchar(255) NOT NULL,
-  `date` text NOT NULL
+  `date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -64,16 +64,16 @@ CREATE TABLE `laptops` (
 --
 
 INSERT INTO `laptops` (`id`, `brand`, `model`, `price`, `processor`, `memory`, `storage`, `graphics`, `date`) VALUES
-(7, 'Asus', 'Asus ROG Zephyrus Duo 16 (2022) GX650', '6309.00', 'AMD Ryzen 9 6900HX 3.3GHz', 64, '8TB SSD + 8TB SSD', 'Nvidia GeForce RTX 3070 Ti', '11/01/2023'),
-(8, 'MSI ', 'MSI GE76 Raider 12UHS 239', '3199.00', 'Intel Core i9-12900HK 2.5GHz', 64, '2TB SSD + 1TB SSD', 'Nvidia GeForce RTX 3080 Ti', '11/01/2023'),
-(9, 'MSI ', 'MSI GT77 Titan 12UHS-006', '3199.00', 'Intel Core i9-12900HX 2.3GHz', 64, '2TB SSD', 'Nvidia GeForce RTX 3080 Ti', '11/01/2023'),
-(10, 'ASUS ', 'ASUS Zenbook Pro 16X ', '2950.00', 'Intel Core i9-12900H 2.5GHz', 32, '2TB SSD', 'Nvidia GeForce RTX 3060', '11/01/2023'),
-(11, 'MSI ', 'MSI GS77 Stealth 12UHS-040', '3199.00', 'Intel Core i9-12900H 1.8GHz', 32, '1TB SDD', 'Nvidia GeForce RTX 3080 Ti', '11/01/2023'),
-(12, 'Asus ', 'Asus ROG Flow X13 (2022) GV301', '1770.00', 'AMD Ryzen 9 6900HS 3.3GHz', 32, '1TB SSD', 'Nvidia GeForce RTX 3050 Ti', '11/01/2023'),
-(13, 'Gigabyte ', 'Gigabyte AERO 16 YE5', '2899.00', 'Intel Core i9-12900HK 2.5GHz', 16, '2TB SSD', 'Nvidia GeForce RTX 3080 Ti', '11/01/2023'),
-(14, 'Asus ', 'Asus ROG Strix Scar 15 (2022) G533', '1984.00', 'Intel Core i9-12900H 2.5GHz', 64, '2TB SSD', 'Nvidia GeForce RTX 3080', '11/01/2023'),
-(15, 'Razer ', 'Razer Blade 17 4K Touch', '3750.00', 'Intel Core i9-11900H 2.5GHz', 32, '1TB SSD', 'Nvidia GeForce RTX 3080', '11/01/2023'),
-(23, 'Gigabyte', 'Gigabyte Aorus 17G YD', '3500.00', 'Intel Core i7-11800H 2.3GHz', 32, '512GB SSD', 'Nvidia GeForce RTX 3080', '15/01/2023');
+(7, 'Asus', 'Asus ROG Zephyrus Duo 16 (2022) GX650', '6309.00', 'AMD Ryzen 9 6900HX 3.3GHz', 64, '8TB SSD + 8TB SSD', 'Nvidia GeForce RTX 3070 Ti', '2023-01-10'),
+(8, 'MSI ', 'MSI GE76 Raider 12UHS 239', '3199.00', 'Intel Core i9-12900HK 2.5GHz', 64, '2TB SSD + 1TB SSD', 'Nvidia GeForce RTX 3080 Ti', '2023-01-10'),
+(9, 'MSI ', 'MSI GT77 Titan 12UHS-006', '3199.00', 'Intel Core i9-12900HX 2.3GHz', 64, '2TB SSD', 'Nvidia GeForce RTX 3080 Ti', '2023-01-10'),
+(10, 'ASUS ', 'ASUS Zenbook Pro 16X ', '2950.00', 'Intel Core i9-12900H 2.5GHz', 32, '2TB SSD', 'Nvidia GeForce RTX 3060', '2023-01-10'),
+(11, 'MSI ', 'MSI GS77 Stealth 12UHS-040', '3199.00', 'Intel Core i9-12900H 1.8GHz', 32, '1TB SDD', 'Nvidia GeForce RTX 3080 Ti', '2023-01-10'),
+(12, 'Asus ', 'Asus ROG Flow X13 (2022) GV301', '1770.00', 'AMD Ryzen 9 6900HS 3.3GHz', 32, '1TB SSD', 'Nvidia GeForce RTX 3050 Ti', '2023-01-10'),
+(13, 'Gigabyte ', 'Gigabyte AERO 16 YE5', '2899.00', 'Intel Core i9-12900HK 2.5GHz', 16, '2TB SSD', 'Nvidia GeForce RTX 3080 Ti', '2023-01-10'),
+(14, 'Asus ', 'Asus ROG Strix Scar 15 (2022) G533', '1984.00', 'Intel Core i9-12900H 2.5GHz', 64, '2TB SSD', 'Nvidia GeForce RTX 3080', '2023-01-10'),
+(15, 'Razer ', 'Razer Blade 17 4K Touch', '3750.00', 'Intel Core i9-11900H 2.5GHz', 32, '1TB SSD', 'Nvidia GeForce RTX 3080', '2023-01-10'),
+(23, 'Gigabyte', 'Gigabyte Aorus 17G YD', '3500.00', 'Intel Core i7-11800H 2.3GHz', 32, '512GB SSD', 'Nvidia GeForce RTX 3080', '2023-01-10');
 
 --
 -- Indexes for dumped tables
