90ba2b9eabe3e9de83d28abbcbe1cf3ed64d12a3
Mahesafy <55169719+mahesafy@users.noreply.github.com>
2023-01-16 10:54:54 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$user = 'rodeom';
-$password = 'RAMADHAN001';
-$dbname = 'laptops';
+$user = 'root';
+$password = '';
+$dbname = 'uas';
 $conn = mysqli_connect($host, $user, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
